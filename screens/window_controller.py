44aadf48c02c97df277e81ea86fde991581426ac
guilhermehrn <guibork@hotmail.com>
2021-10-04 18:35:49 -0300

    refatoração do prisma

--- screens/window_controller.py
+++ screens/window_controller.py
@@ -3,6 +3,7 @@ import os.path
 
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.uic import loadUi
+from ..settings.config_window import ConfigWindow
 
 class MainWindow (QtWidgets.QDialog):
 
@@ -33,25 +34,7 @@ class MainWindow (QtWidgets.QDialog):
     def go_to_shapefile(self):
         self.switch_overlay_shapefile.emit()
 
-class ConfigWindow (QtWidgets.QDialog):
 
-    back_window = QtCore.pyqtSignal()
-    continue_window = QtCore.pyqtSignal()
-
-    def __init__(self):
-        super(ConfigWindow, self).__init__()
-        loadUi(os.path.join(os.path.dirname(__file__), 'config_window.ui'), self)
-
-        self.btn_voltar.clicked.connect(self.back)
-        self.btn_continuar.clicked.connect(self.next)
-
-    def back(self):
-        self.hide()
-        self.back_window.emit()
-
-    def next(self):
-        self.hide()
-        self.continue_window.emit()
 
 class OverlayAddress (QtWidgets.QDialog):
 
