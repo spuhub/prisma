e425dc9fb87fd5420d97301c20298c075293710b
vinirafaelsch <vinirafaelsch@gmail.com>
2022-12-02 21:53:51 -0300

    pre config default layers

--- utils/default_sld.py
+++ utils/default_sld.py
@@ -1,10 +1,11 @@
+import os
 from enum import Enum
 
 class slddefaultlayers(Enum):
-    POLYGON_SLD_INPUT = "C:\\Users\\vinir\\Desktop\\sld\\3_2_2_Trecho_Terreno_Marinha_A.sld"
-    LINE_SLD_INPUT = "C:\\Users\\vinir\\Desktop\\sld\\1_6_5_Trecho_Duto_L.sld"
-    POINT_SLD_INPUT = "C:\\Users\\vinir\\Desktop\\sld\\1_6_1_Estrut_Apoio_Transporte_P.sld"
-    BUFFER_SLD = "C:\\Users\\vinir\\Desktop\\sld\\1_2_3_Massa_Dagua_A.sld"
-    POLYGON_SLD_OVERLAY = "C:\\Users\\vinir\\Desktop\\sld\\1_5_6_Elemento_Fisiografico_Natural_A.sld"
-    LINE_SLD_OVERLAY = "C:\\Users\\vinir\\Desktop\\sld\\1_5_8_Curva_Nivel_L.sld"
-    POINT_SLD_OVERLAY = "C:\\Users\\vinir\\Desktop\\sld\\1_6_2_Estacao_Ferroviario_P.sld"
+    POLYGON_SLD_INPUT = f"{os.path.dirname(__file__)}/../styles/3_2_2_Trecho_Terreno_Marinha_A.sld"
+    LINE_SLD_INPUT = f"{os.path.dirname(__file__)}/../styles/1_6_5_Trecho_Duto_L.sld"
+    POINT_SLD_INPUT = f"{os.path.dirname(__file__)}/../styles/1_6_1_Estrut_Apoio_Transporte_P.sld"
+    BUFFER_SLD = f"{os.path.dirname(__file__)}/../styles/1_2_3_Massa_Dagua_A.sld"
+    POLYGON_SLD_OVERLAY = f"{os.path.dirname(__file__)}/../styles/1_5_6_Elemento_Fisiografico_Natural_A.sld"
+    LINE_SLD_OVERLAY = f"{os.path.dirname(__file__)}/../styles/1_5_8_Curva_Nivel_L.sld"
+    POINT_SLD_OVERLAY = f"{os.path.dirname(__file__)}/../styles/1_6_2_Estacao_Ferroviario_P.sld"
