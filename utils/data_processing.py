0aa97fdf20c938de6950c040d3a666c8ad5605c6
Marco A. Reliquias <marcoaurelio.reliquias@gmail.com>
2023-02-23 19:43:43 -0300

    Ajuste na execução de funções e processamento de identificação de zonas

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -2,6 +2,7 @@ from ..databases.db_connection import DbConnection
 from ..databases.handle_selections import HandleSelections
 from .utils import Utils
 from .lyr_utils import *
+from qgis.core import QgsVectorFileWriter
 
 class DataProcessing():
     def __init__(self):
@@ -12,8 +13,7 @@ class DataProcessing():
     def data_preprocessing(self, operation_config):
         # Leitura do shapefile de input
         lyr_input = operation_config['input']
-        lyr_reproj = layer_reproject(lyr_input, 4326)
-        lyr_reproj.setName(lyr_input.name())
+        lyr_input = lyr_input_process(lyr_input, 4326)
 
         # Leitura de itens de comparação
         dic_lyr_retorno = {}
@@ -22,6 +22,6 @@ class DataProcessing():
         list_selected_wfs = self.handle_selections.read_selected_wfs(operation_config['wfs'])
         list_selected_db = self.handle_selections.read_selected_db(operation_config['pg'])
 
-        dic_lyr_retorno = {'input': lyr_reproj, 'required': list_required, 'db': list_selected_db, 'shp': list_selected_shp, 'wfs': list_selected_wfs}
+        dic_lyr_retorno = {'input': lyr_input_process, 'required': list_required, 'db': list_selected_db, 'shp': list_selected_shp, 'wfs': list_selected_wfs}
 
         return dic_lyr_retorno
--- utils/lyr_utils.py
+++ utils/lyr_utils.py
@@ -1,7 +1,8 @@
+import os
 from qgis import processing
 from qgis.core import QgsVectorLayer
 
-def layer_reproject(layer_in:QgsVectorLayer, crs_out:int=0) -> QgsVectorLayer:
+def layer_reproject(layer_in:QgsVectorLayer, crs_out:int=4326) -> QgsVectorLayer:
     '''
         Função de apoio para execução de ferramenta de reprojeção do QGIS.
             Parameters:
@@ -15,10 +16,9 @@ def layer_reproject(layer_in:QgsVectorLayer, crs_out:int=0) -> QgsVectorLayer:
                  'TARGET_CRS': f'EPSG:{crs_out}',
                  'OUTPUT': 'memory:'
                 }
-    lyr_reproj = processing.run('native:reprojectlayer', parameter)['OUTPUT']
-    lyr_fixed = layer_fix_geometries(lyr_reproj)
-    
-    return lyr_fixed
+    lyr_return = processing.run('native:reprojectlayer', parameter)['OUTPUT']
+
+    return lyr_return
     
 def layer_fix_geometries(layer_in:QgsVectorLayer) -> QgsVectorLayer:
     '''
@@ -32,4 +32,42 @@ def layer_fix_geometries(layer_in:QgsVectorLayer) -> QgsVectorLayer:
     parameter = {'INPUT': layer_in,
                  'OUTPUT': 'memory:'
                 }
-    return processing.run('native:fixgeometries', parameter)['OUTPUT']
+    lyr_return = processing.run('native:fixgeometries', parameter)['OUTPUT']
+
+    return lyr_return
+
+def layer_get_sirgas_epsg(layer_in:QgsVectorLayer) -> QgsVectorLayer:
+    '''
+        Função de apoio para identificação de Zona Sirgas 2000.
+            Parameters:
+                layer_in (QgsVectorLayer): Objeto QgsVectorLayer de entrada
+                
+            Returns:
+                Memory_out (QgsVectorLayer): Objeto QgsVectorLayer em memória do objeto com as zonas
+    '''
+    shp_zonas_sirgas = os.path.join(os.path.dirname(os.path.dirname(__file__)), 
+                                    'shapefiles', 
+                                    'Zonas_UTM_BR-EPSG4326.shp')
+    lyr_zonas_sirgas = QgsVectorLayer(shp_zonas_sirgas, 'Zonas_Sirgas', 'ogr')
+    parameter = {'INPUT': layer_in,
+                 'JOIN': lyr_zonas_sirgas,
+                 'PREDICATE':[0],
+                 'JOIN_FIELDS':['EPSG_S2000'],
+                 'METHOD':2,
+                 'DISCARD_NONMATCHING':False,
+                 'PREFIX':'',
+                 'OUTPUT':'memory:'}
+    lyr_return = processing.run("native:joinattributesbylocation", parameter)['OUTPUT']
+
+    return lyr_return
+
+
+def lyr_input_process(layer_in:QgsVectorLayer, crs_out:int=4326) -> QgsVectorLayer:
+    lyr_reproj = layer_reproject(layer_in, crs_out)
+
+    lyr_fixed = layer_fix_geometries(lyr_reproj)
+
+    lyr_return = layer_get_sirgas_epsg(lyr_fixed)
+    lyr_return.setName(layer_in.name())
+
+    return lyr_return
