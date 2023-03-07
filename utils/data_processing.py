91204490d636c7a1f9ad4aa8378bf8532f76beed
vinirafaelsch <vinirafaelsch@gmail.com>
2023-03-07 12:11:51 -0300

    adicao de buffer

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -14,27 +14,34 @@ class DataProcessing():
         # Leitura do shapefile de input
         lyr_input = operation_config['input']
         lyr_input = lyr_process(lyr_input, 4326)
+        input_buffer = operation_config.get('aproximacao', {}).get('input', {})
 
         # Leitura de itens de comparação
         dic_lyr_retorno = {}
-        list_required = self.handle_selections.read_required_layers(operation_config['obrigatorio'])
-        list_selected_shp = self.handle_selections.read_selected_shp(operation_config['shp'])
-        list_selected_wfs = self.handle_selections.read_selected_wfs(operation_config['wfs'])
-        list_selected_db = self.handle_selections.read_selected_db(operation_config['pg'])
-
-        # Adiciona buffer na camada de input
-        lyr_input_buffer = insert_buffer(lyr_input, 100)
+        list_required, operation_config = self.handle_selections.read_required_layers(operation_config['obrigatorio'], operation_config)
+        list_selected_shp, operation_config = self.handle_selections.read_selected_shp(operation_config['shp'], operation_config)
+        list_selected_wfs, operation_config = self.handle_selections.read_selected_wfs(operation_config['wfs'], operation_config)
+        list_selected_db, operation_config = self.handle_selections.read_selected_db(operation_config['pg'], operation_config)
 
         # Adiciona buffer nas camadas de comparação
         for layer in list_selected_shp:
-            layer = insert_buffer(layer, 100)
+            if operation_config.get('aproximacao') and operation_config['aproximacao'].get(layer.name()):
+                layer = insert_buffer(layer, operation_config['aproximacao'][layer.name()][0])
 
         for layer in list_selected_wfs:
-            layer = insert_buffer(layer, 100)
+            if operation_config.get('aproximacao') and operation_config['aproximacao'].get(layer.name()):
+                layer = insert_buffer(layer, operation_config['aproximacao'][layer.name()])
 
         for layer in list_selected_db:
-            layer = insert_buffer(layer, 100)
+            if operation_config.get('aproximacao') and operation_config['aproximacao'].get(layer.name()):
+                layer = insert_buffer(layer, operation_config['aproximacao'][layer.name()])
 
-        dic_lyr_retorno = {'input': lyr_input_buffer, 'input_default': lyr_input, 'required': list_required, 'db': list_selected_db, 'shp': list_selected_shp, 'wfs': list_selected_wfs}
+
+        dic_lyr_retorno = {'input': lyr_input, 'required': list_required, 'db': list_selected_db, 'shp': list_selected_shp, 'wfs': list_selected_wfs}
+        
+        # Trata o retorno da função caso usuário tenha inserido buffer na camada de entrada
+        if input_buffer:
+            lyr_input_buffer = insert_buffer(lyr_input, input_buffer)
+            dic_lyr_retorno.update(input = lyr_input_buffer, input_default = lyr_input)
 
         return dic_lyr_retorno
