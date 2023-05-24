aef2d62caa26a58a74d6250e5f342531b4a35774
Marco Reliquias <marcoaurelio.reliquias@gmail.com>
2023-05-24 13:29:17 -0300

    Finalização da primeira etapa de conversão de memorial 'textual'

--- screens/memorial_conversion.py
+++ screens/memorial_conversion.py
@@ -1,14 +1,12 @@
 import os.path
 
-from qgis.core import QgsVectorLayer, QgsCoordinateReferenceSystem
-
+from qgis.core import QgsFeature, QgsVectorLayer, QgsGeometry,  QgsPointXY, QgsCoordinateReferenceSystem, QgsProject
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5.uic import loadUi
 
 import PyPDF2
 import os, re
-from osgeo import ogr
 
 
 class MemorialConversion (QtWidgets.QDialog):
@@ -51,34 +49,65 @@ class MemorialConversion (QtWidgets.QDialog):
         if (self.path_input != ""):
             
             lista_coords = self.extrai_coords(self.path_input)
+            lyr_pdf = self.gerar_layer(lista_coords)
+
+            self.hide()
 
         else:
             self.iface.messageBar().pushMessage("Error", "Selecione um pdf de entrada.", level=1)
 
     def extrai_coords(self, arq_pdf):
+        """
+            Função que extrai coordenadas de pdf's usando REGEX
+        """
         pdfFileObj = open(arq_pdf,'rb')
 
         pdf_reader = PyPDF2.PdfFileReader(pdfFileObj)
         totalPages = pdf_reader.numPages
         text_out = ""
+        regex_str = self.get_regex_from_str(self.ex_coords)
 
         for i in range(0, totalPages):
             pages = pdf_reader.getPage(i)
             text = pages.extractText()
             text_out += text
 
-        lista_coords = re.findall(r'E\s\d\d\d\.\d\d\d,\d\d,\sN \d\.\d\d\d\.\d\d\d,\d\d', text_out)
+        lista_coords = re.findall(regex_str, text_out)
+
 
         return lista_coords
     
-    def gerar_geom(self, lista):
-        ring = ogr.Geometry(ogr.wkbLinearRing)
-        poly = ogr.Geometry(ogr.wkbPolygon)
+    def gerar_layer(self, lista):
+        """
+            Função para geração de layer de geometria extraida, a partir de coordenadas
+        """
+        mem_layer = QgsVectorLayer(f"Polygon?crs={self.select_crs.crs().authid().lower()}", "Memorial Descritivo - PDF", "Memory")
+        QgsProject.instance().addMapLayer(mem_layer)
+        poly_coords = []
 
         for idx, coords in enumerate(lista):
             latitude = float(coords.split(', ')[1].replace(".", "").replace("N ", "").replace(",","."))
             longitude = float(coords.split(', ')[0].replace(".", "").replace("E ", "").replace(",","."))
-            ring.AddPoint(longitude, latitude)
+            poly_coords.append((longitude, latitude))
 
-        poly.AddGeometry(ring)
-        
+        polygon = QgsGeometry.fromPolygonXY( [[ QgsPointXY( pair[0], pair[1] ) for pair in poly_coords ]])
+        feature = QgsFeature()
+        feature.setGeometry(polygon)
+        mem_layer.dataProvider().addFeatures([feature])
+
+        return mem_layer
+
+    def get_regex_from_str(self, string):
+        """
+            Função que extrai padrão REGEX a partir de par de coordenada passado como exemplo
+        """
+        str_return = ""
+        for character in string:
+            if character.isdigit():
+                str_return += '\d'
+            elif character.isspace():
+                str_return += '\s'
+            else:
+                str_return += character
+        return str_return
+            
