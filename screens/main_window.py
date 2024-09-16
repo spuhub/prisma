a7100f8d2a256aa0f235f531143e3ae02f855d2a
vinirafaelsch <vinirafaelsch@gmail.com>
2024-09-15 23:18:27 -0300

    ajuste empacotamento

--- screens/main_window.py
+++ screens/main_window.py
@@ -41,8 +41,8 @@ class MainWindow (QtWidgets.QDialog):
             # ret = msg.question(self, 'Download Dependências ', "É necessario fazer o download de algumas dependências para o Prisma funcionar corretamente! Deseja fazer o Download das Dependências?", QMessageBox.Yes | QMessageBox.Cancel)
             # if ret == QMessageBox.Yes:
             #    instala_dependencias(arq_dependencias)
-            #    with open(flag_dependencias, "w", encoding='utf-8') as file:
-            #        file.write("True")
+            with open(flag_dependencias, "w", encoding='utf-8') as file:
+                file.write("True")
                  
             #    ret = msg.question(self, 'Reinicio do Sistema Necessário', "É necessario fechar o QGIS para aplicar as instalações", QMessageBox.Ok)
             #    sys.exit()
